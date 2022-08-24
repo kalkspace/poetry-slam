2e0bed8924175b1ab8ac57e82d685b8be828e2f7
Andreas Streichardt <andreas@mop.koeln>
2022-08-24 08:24:31 +0200

    ✨ introduce a real thread for printing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 #[macro_use]
 extern crate rocket;
 
-use std::error::Error;
+use std::{error::Error, thread::spawn};
 
 use flume::{Receiver, Sender};
 use printer::PoetryPrinter;
-use rocket::{form::Form, tokio::spawn, Build, Rocket, State};
+use rocket::{form::Form, Build, Rocket, State};
 use rocket_dyn_templates::{context, Template};
 
 mod poem_generator;
 mod printer;
 mod training_data;
+
+struct PrinterArgs {
+    name: String,
+    poem: String,
+}
+
+struct Print {
+    join_handle: std::thread::JoinHandle<()>,
+    poem_tx: Sender<PrinterArgs>,
+}
 
 #[derive(FromForm)]
 struct PoemGenerationForm<'r> {
@@ -33,7 +43,7 @@ fn index() -> Template {
 #[post("/", data = "<poem_generation>")]
 async fn generate(
     poem_generation: Form<PoemGenerationForm<'_>>,
-    poem_tx: &State<Option<Sender<(String, String)>>>,
+    poem_tx: &State<Option<Sender<PrinterArgs>>>,
 ) -> Result<Template, String> {
     // hmm. this generates a 200 in case of an error :S
     let poem = poem_generator::generate(poem_generation.training_data)
@@ -43,7 +53,10 @@ async fn generate(
     let poem = if poem_generation.print_and_hide {
         if let Some(poem_tx) = poem_tx.inner() {
             poem_tx
-                .send((poem_generation.name.to_string(), poem))
+                .send(PrinterArgs {
+                    name: poem_generation.name.to_string(),
+                    poem,
+                })
                 .map_err(|e| e.to_string())?;
         }
         None
@@ -61,7 +74,7 @@ async fn generate(
     ))
 }
 
-fn rocket(poem_tx: Option<Sender<(String, String)>>) -> Rocket<Build> {
+fn rocket(poem_tx: Option<Sender<PrinterArgs>>) -> Rocket<Build> {
     rocket::build()
         .mount("/", routes![index, generate])
         .manage(poem_tx)
@@ -70,26 +83,35 @@ fn rocket(poem_tx: Option<Sender<(String, String)>>) -> Rocket<Build> {
 
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let printer = match PoetryPrinter::new() {
+    let print = match PoetryPrinter::new() {
         Ok(mut printer) => {
-            let (poem_tx, poem_rx): (Sender<(String, String)>, Receiver<(String, String)>) =
+            let (poem_tx, poem_rx): (Sender<PrinterArgs>, Receiver<PrinterArgs>) =
                 flume::unbounded();
-            let print_task = spawn(async move {
-                loop {
-                    let (name, training_data) = poem_rx.recv().unwrap();
-                    printer.print_poem(&name, &training_data).unwrap();
+            let join_handle = spawn(move || loop {
+                let args = match poem_rx.recv() {
+                    Ok(args) => args,
+                    Err(e) => {
+                        println!("Printer thread exited : {}", e);
+                        return;
+                    }
+                };
+                if let Err(e) = printer.print_poem(&args.name, &args.poem) {
+                    println!("Printing failed : {}", e);
                 }
             });
-            Some((poem_tx, print_task))
+            Some(Print {
+                join_handle,
+                poem_tx,
+            })
         }
         Err(e) => {
             eprintln!("Printer init failed: {}. Skipping print.", e);
             None
         }
     };
-    if let Some(printer) = printer {
-        let _ = rocket(Some(printer.0)).launch().await;
-        printer.1.abort();
+    if let Some(print) = print {
+        let _ = rocket(Some(print.poem_tx)).launch().await;
+        print.join_handle.join().unwrap();
     } else {
         let _ = rocket(None).launch().await;
     }
